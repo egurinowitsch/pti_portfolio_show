0249bcdbcde849b1d8c090919e8788b286340d7a
egurinowitsch <egurinowitsch@yahoo.com>
2013-05-29 22:45:57 -0400

    pti portfolio show website

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// JavaScript Document
+onload = init;
+
+function init(){
+	$(function () {
+  $("#fixed-bar")
+    .css({position:'fixed',bottom:'0px'})
+    .hide();
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 300) {
+      $('#fixed-bar').fadeIn(200);
+    } else {
+      $('#fixed-bar').fadeOut(200);
+    }
+  });
+  $('.go-top').click(function () {
+    $('html,body').animate({
+      scrollTop: 0
+    }, 1000);
+    return false;
+  });
+});
+}
